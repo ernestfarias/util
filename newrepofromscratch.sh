a061b169c6514a680021763d9ee4fed364097c01
root <root@localhost>
2018-11-07 21:38:28 +0000

    upd

--- newrepofromscratch.sh
+++ newrepofromscratch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
 USERNAME=ernestfarias
-REPONAME=util
+REPONAME="${PWD##*/}"
 echo "GitHub create new repo from bash - eaf"
 #echo "{name:"${REPONAME}"}"
 #should looks like  curl -u ernestfarias https://api.github.com/user/repos -d '{"name":"util"}'
@@ -11,8 +11,8 @@ curl -u $USERNAME https://api.github.com/user/repos -d '{"name":"'${REPONAME}'"}
 #push contents
 echo '"# -"'$REPONAME'"' >> README.md
 git init
-git add README.md
+git add *
 git commit -m "first commit"
-git remote add origin https://github.com/ernestfarias/-REPONAME.git
+git remote add origin https://github.com/ernestfarias/$REPONAME.git
 git push -u origin master
 
